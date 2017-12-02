f26bd883863c9677b8463ab58f68af60fb0c9a52
Saltpile123 <33727726+Saltpile123@users.noreply.github.com>
2017-12-02 12:36:57 +0100

    Initial commit

---
+++ MandelBrot/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MandelBrot
+{
+    partial class Mandelform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MandelPic = new System.Windows.Forms.PictureBox();
+            this.xMid = new System.Windows.Forms.TextBox();
+            this.yMid = new System.Windows.Forms.TextBox();
+            this.Herhalingen = new System.Windows.Forms.TextBox();
+            this.Zoom = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MandelPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MandelPic
+            // 
+            this.MandelPic.Location = new System.Drawing.Point(12, 89);
+            this.MandelPic.Name = "MandelPic";
+            this.MandelPic.Size = new System.Drawing.Size(610, 610);
+            this.MandelPic.TabIndex = 0;
+            this.MandelPic.TabStop = false;
+            // 
+            // xMid
+            // 
+            this.xMid.Location = new System.Drawing.Point(13, 13);
+            this.xMid.Name = "xMid";
+            this.xMid.Size = new System.Drawing.Size(100, 20);
+            this.xMid.TabIndex = 1;
+            this.xMid.Text = "0";
+            // 
+            // yMid
+            // 
+            this.yMid.Location = new System.Drawing.Point(13, 40);
+            this.yMid.Name = "yMid";
+            this.yMid.Size = new System.Drawing.Size(100, 20);
+            this.yMid.TabIndex = 2;
+            this.yMid.Text = "0";
+            // 
+            // Herhalingen
+            // 
+            this.Herhalingen.Location = new System.Drawing.Point(120, 13);
+            this.Herhalingen.Name = "Herhalingen";
+            this.Herhalingen.Size = new System.Drawing.Size(100, 20);
+            this.Herhalingen.TabIndex = 3;
+            this.Herhalingen.Text = "100";
+            // 
+            // Zoom
+            // 
+            this.Zoom.Location = new System.Drawing.Point(120, 40);
+            this.Zoom.Name = "Zoom";
+            this.Zoom.Size = new System.Drawing.Size(100, 20);
+            this.Zoom.TabIndex = 4;
+            this.Zoom.Text = "5";
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(227, 9);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 5;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(227, 36);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 6;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            // 
+            // Mandelform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 711);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.Zoom);
+            this.Controls.Add(this.Herhalingen);
+            this.Controls.Add(this.yMid);
+            this.Controls.Add(this.xMid);
+            this.Controls.Add(this.MandelPic);
+            this.Name = "Mandelform";
+            this.Text = "MandelApp";
+            ((System.ComponentModel.ISupportInitialize)(this.MandelPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MandelPic;
+        private System.Windows.Forms.TextBox xMid;
+        private System.Windows.Forms.TextBox yMid;
+        private System.Windows.Forms.TextBox Herhalingen;
+        private System.Windows.Forms.TextBox Zoom;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ MandelBrot/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MandelBrot
+{
+    public partial class Mandelform : Form
+    {
+        public Mandelform()
+        {
+            InitializeComponent();
+
+        }
+
+        //private double startX, startY, curX, curY;
+        private double m_Xmin, m_Xmax, m_Ymin, m_Ymax;
+
+        public int MaxHerhalingen;
+        public int zoom;
+        //public double Zr, Zim, Z2r, Z2im;
+
+        private Bitmap MandelBit;
+
+        private const double minX = -2.2;
+        private const double maxX = 1;
+        private const double minY = -1.2;
+        private const double maxY = 1.2;
+
+        private void AdjustAspect()
+        {
+            double hgt, wid, mid;
+
+            double want_aspect = (m_Ymax - m_Ymin) / (m_Xmax - m_Xmin);
+            double picCanvas_aspect = MandelPic.ClientSize.Height / (double)MandelPic.ClientSize.Width;
+            if (want_aspect > picCanvas_aspect)
+            {
+                // The selected area is too tall and thin.
+                // Make it wider.
+                wid = (m_Ymax - m_Ymin) / picCanvas_aspect;
+                mid = (m_Xmin + m_Xmax) / 2;
+                m_Xmin = mid - wid / 2;
+                m_Xmax = mid + wid / 2;
+            }
+            else
+            {
+                // The selected area is too short and wide.
+                // Make it taller.
+                hgt = (m_Xmax - m_Xmin) * picCanvas_aspect;
+                mid = (m_Ymin + m_Ymax) / 2;
+                m_Ymin = mid - hgt / 2;
+                m_Ymax = mid + hgt / 2;
+            }
+        }
+
+        public void OK_Click(object sender, EventArgs e)
+        {
+            MaxHerhalingen = int.Parse(Herhalingen.Text);
+            zoom = int.Parse(Zoom.Text);
+            DrawMandel();
+        }
+
+        private void DrawMandel()
+        {
+            const int maxd = 4;
+            MandelBit = new Bitmap(MandelPic.Width, MandelPic.Height);
+            Graphics gr = Graphics.FromImage(MandelBit);
+
+            gr.Clear(MandelPic.BackColor);
+            MandelPic.Image = MandelBit;
+            Application.DoEvents();
+
+            AdjustAspect();
+           
+            int wid = MandelPic.Width;
+            int hgt = MandelPic.Height;
+            double dReaC = zoom * (m_Xmax - m_Xmin) / (wid - 1);
+            double dImaC = zoom * (m_Ymax - m_Ymin) / (hgt - 1);
+
+            //int num_colors = Colors.Count;
+            double ReaC = m_Xmin;
+            for (int X = 0; X < wid; X++)
+            {
+                double ImaC = m_Ymin;
+                for (int Y = 0; Y < hgt; Y++)
+                {
+                    double ReaZ = 0; //Zr;
+                    double ImaZ = 0; //Zim;
+                    double ReaZ2 = 0; //Z2r;
+                    double ImaZ2 = 0; //Z2im;
+                    int it = 1;
+                    do
+                    {
+                        // Calculate Z(clr).
+                        ReaZ2 = ReaZ * ReaZ;
+                        ImaZ2 = ImaZ * ImaZ;
+                        ImaZ = 2 * ImaZ * ReaZ + ImaC;
+                        ReaZ = ReaZ2 - ImaZ2 + ReaC;
+                        it++;
+                    } while ((it < MaxHerhalingen) && (ReaZ2 + ImaZ2 < maxd));
+                    //Console.WriteLine(ImaZ + "+" + ReaZ);
+                    // Set the pixel's value.
+                    if (it < MaxHerhalingen)
+                        if (it % 2 != 0)
+                            MandelBit.SetPixel(X, Y, Color.Black);
+                        else
+                            MandelBit.SetPixel(X, Y, Color.White);
+                    else
+                        MandelBit.SetPixel(X, Y, Color.Black);
+
+                    ImaC += dImaC;
+                }
+                ReaC += dReaC;
+
+                if (X % 10 == 0) MandelPic.Refresh();
+            }
+
+        }
+
+
+    }
+}
